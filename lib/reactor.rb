ea994ed73cc709d4d4ebd643cc6299417b98e9a1
Jack Wilson <wils3005@gmail.com>
2020-01-25 13:37:51 -0500

    lib/reactor.rb | 19 ++++++++++---------  1 file changed, 10 insertions(+), 9 deletions(-)

--- lib/reactor.rb
+++ lib/reactor.rb
@@ -4,9 +4,10 @@ puts __FILE__
 
 module Reactor
   MAXLEN = 16_384
-  READ = []
-  WRITE = []
-  ERROR = []
+
+  READ = [].freeze
+  WRITE = [].freeze
+  ERROR = [].freeze
 
   ReactorRegistrationError = Class.new(IOError)
   ReactorCallbackError = Class.new(IOError)
@@ -17,21 +18,21 @@ module Reactor
     end
   end
 
-  def self.included(klass)
-    klass.extend(ClassMethods)
+  def self.included(mod)
+    mod.extend(ClassMethods)
   end
 
   def register(mode, callback_name)
     @mode = mode << self
     @callback_name = "_reactor_#{callback_name}"
-  rescue NameError
-    raise(ReactorRegistrationError, inspect)
+  rescue => e
+    puts ReactorRegistrationError.new(e.inspect)
   end
 
   def reactor_callback
     __send__(@callback_name)
-  rescue NoMethodError
-    raise(ReactorCallbackError, inspect)
+  rescue => e
+    puts ReactorCallbackError.new(e.inspect)
     unregister
   end
 
