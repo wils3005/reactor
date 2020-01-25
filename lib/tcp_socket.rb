de469efef335f6e448cc96e7e968d0d40b09a8a9
Jack Wilson <wils3005@gmail.com>
2020-01-25 16:41:58 -0500

    lib/tcp_socket.rb | 9 ++++-----  1 file changed, 4 insertions(+), 5 deletions(-)

--- lib/tcp_socket.rb
+++ lib/tcp_socket.rb
@@ -10,13 +10,12 @@ class TCPSocket
       Reactor::READ << self
       Fiber.yield
       request = read_nonblock(MAXLEN)
-      close_read
       Reactor::READ.delete(self)
-      Reactor::WRITE << self
-      Fiber.yield
+      # Reactor::WRITE << self
+      # Fiber.yield
       write_nonblock(Response.new(request))
-      close_write
-      Reactor::WRITE.delete(self)
+      close
+      # Reactor::WRITE.delete(self)
     end
   end
 end
